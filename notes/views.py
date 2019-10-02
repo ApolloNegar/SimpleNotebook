04192f0a71906308e6003eb5f427e9fdcf805bb7
Negar <negargaming@gmail.com>
2019-10-02 22:53:28 +0330

    CSS AND STYLES EDITED; PROFILE VIEW DISABLED; TABLE VIEW CREATED

--- notes/views.py
+++ notes/views.py
@@ -35,7 +35,9 @@ class UserPostListView(ListView):
     paginate_by = 3
 
     def get_queryset(self):
+
         user = get_object_or_404(User, username=self.kwargs.get('username'))
+
         return Post.objects.filter(author=user).order_by('-date_posted')
 
 
